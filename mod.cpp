bcf316e6562b371612c332445cbacf65e19545d5
Marc13Bautista <35089747+Marc13Bautista@users.noreply.github.com>
2024-05-24 11:01:02 -1000

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+name 			= "Arma 3 Realistic Names [UPDATED]";
+author 		    = "Marc🦈";
+logo 			= "dta\logo.paa";
+logoOver 		= "dta\overlogo.paa";
+tooltip 		= "Arma 3 Realistic Names [UPDATED]";
+tooltipOwned 	= "Arma 3 Realistic Names [UPDATED]";
+picture 		= "dta\picture.paa";
+actionName 	    = "Workshop Page";
+action 		    = "https://steamcommunity.com/sharedfiles/filedetails/?id=3248094475";
+overview 		= "This mod changes all names of gear and vehicles to their real life counterparts (including Western Sahara and Reaction Forces CDLC). Thanks to ACE3 and blackfisch for the foundational work and Axle for the guidance.";
+hideName 		= 0;
+hidePicture	    = 0;	// Hide the extension menu
+dlcColor[] 	    = {0.298,0.592,0.306,1};
+logoSmall 		= "dta\logosmall.paa";
